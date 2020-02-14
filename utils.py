7db6cecd876891e94fc8b98ffd821b929274182d
payam <pkhorram@ucsd.edu>
2020-02-14 11:39:11 -0800

    new updates

--- utils.py
+++ utils.py
@@ -48,7 +48,7 @@ def iou(pred, labels):
     
     # This function returns a list that contains the iou for each class over a batch of images
     
-    print(labels_classes)
+    
     ious = []
     n_class = pred.shape[1]
     
